67e087a1f4c24ebaf8ef41ede2d6a135205f4d5d
hardip <hardip.chavda@aditmicrosys.com>
2019-10-11 17:27:40 +0530

    updated on 11102019_0530

--- app/src/main/java/com/vbs/irmenergy/activity/VerifyPaymentDetailActivity.java
+++ app/src/main/java/com/vbs/irmenergy/activity/VerifyPaymentDetailActivity.java
@@ -1,7 +1,6 @@
 package com.vbs.irmenergy.activity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -10,14 +9,13 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.vbs.irmenergy.R;
 
-public class PaymentDetailActivity extends AppCompatActivity implements View.OnClickListener {
+public class VerifyPaymentDetailActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Context mContext;
-    private Button btn_next;
+    private Button btn_save;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -26,7 +24,7 @@ public class PaymentDetailActivity extends AppCompatActivity implements View.OnC
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_payment_detail);
+        setContentView(R.layout.activity_verify_payment_detail);
         mContext = this;
 
         initUI();
@@ -34,16 +32,14 @@ public class PaymentDetailActivity extends AppCompatActivity implements View.OnC
     }
 
     private void initUI() {
-        btn_next = findViewById(R.id.btn_cust_next);
-        btn_next.setOnClickListener(this);
+        btn_save = findViewById(R.id.btn_cust_verify);
+        btn_save.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_cust_next:
-                Intent intent = new Intent(mContext,VerifyPaymentDetailActivity.class);
-                startActivity(intent);
+            case R.id.btn_cust_verify:
                 break;
             default:
                 break;
